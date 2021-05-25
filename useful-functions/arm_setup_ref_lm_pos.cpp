e1d8d7090c31318e50bae37cc2463fbd854087a8
NW_W0303@outlook.com <NW_W0303@outlook.com>
2021-05-25 20:39:19 +0800

    edited from IPC1 05/24

--- useful-functions/arm_setup_ref_lm_pos.cpp
+++ useful-functions/arm_setup_ref_lm_pos.cpp
@@ -75,7 +75,7 @@ int main()
     //
     // @@ input: arm_mission_config_id
 
-    int arm_mission_config_id = 6105;
+    int arm_mission_config_id = 7102;
 
     sql_ptr_->InsertRefLandmarkPos(arm_mission_config_id,lm_pos);
 
